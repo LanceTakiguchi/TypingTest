a8163871f77992c7aacd0eeb72ad303e9e7c6fae
Lance Takiguchi <lancetakiguchi@gmail.com>
2025-07-02 15:05:01 -0700

    replace filler HTML typing text

--- typing-test-app/src/app/page.tsx
+++ typing-test-app/src/app/page.tsx
@@ -6,10 +6,11 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col">
           <div className="flex">
-            <h1>Typing Test</h1>
+            <h1 className="text-4xl">Typing Test</h1>
           </div>
-          <div id="prompt" className="flex">
-            filler prompt texts
+          <div id="prompt" className="flex text-xl">
+            But whatever gain I had, I counted as loss for the sake of Christ. 
+            {/* 8 Indeed, I count everything as loss because of cthe surpassing worth of dknowing Christ Jesus my Lord. For his sake I ehave suffered the loss of all things and count them as rubbish, in order that I may gain Christ 9 and be found in him, not having fa righteousness of my own that comes from the law, but gthat which comes through faith in Christ, the righteousness from God that depends on faith— 10 hthat I may know him and ithe power of his resurrection, and jmay share his sufferings, becoming like him in his death, 11 that by any means possible I may kattain the resurrection from the dead. */}
           </div>
           <div id="entry" className="flex">
             <input className="bg-gray-50 text-black"/>
